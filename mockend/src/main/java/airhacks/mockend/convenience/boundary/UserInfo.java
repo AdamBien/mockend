f10b83c1efc84150a2249f23a47f9fb4b160c605
Adam Bien <abien@adam-bien.com>
2021-05-03 09:01:20 +0200

    additional startup hints added

--- mockend/src/main/java/airhacks/mockend/convenience/boundary/UserInfo.java
+++ mockend/src/main/java/airhacks/mockend/convenience/boundary/UserInfo.java
@@ -1,18 +1,40 @@
 package airhacks.mockend.convenience.boundary;
 
+import java.util.logging.Logger;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.quarkus.runtime.Startup;
 
 @Startup
 @ApplicationScoped
 public class UserInfo {
+
+    @Inject
+    @ConfigProperty(name = "delay-header-name",defaultValue = "delay-in-ms")
+    String delayHeaderName;
+    
+    @Inject
+    @ConfigProperty(name="delay-in-ms", defaultValue="0")
+    Long delayInMs;
+
+    Logger LOG = Logger.getLogger(UserInfo.class.getName());
     
 
     @PostConstruct
     public void printMessages() {
-        System.out.println("use delay-in-ms header to delay all methods");
-        System.out.println("use -Ddelay-in-ms=10 to set the delay from the CLI. Is going to be overriden by headers");
+        LOG.info("----------------------------");
+        LOG.info("You can use: delay-header-name property to change the header name, default is: delay-in-ms");
+        LOG.info("use " + this.delayHeaderName + " header to delay all methods");
+        LOG.info("use -Ddelay-in-ms=10 to set the delay from the CLI. Is going to be overriden by headers");
+        if (this.delayInMs == 0)
+            LOG.info("delay is not set or delay-in-ms=0");
+        LOG.info("e.g. java -jar -Ddelay-in-ms=10 -jar mockend-runner.jar");
+        LOG.info("----------------------------");
+
     }
 }
