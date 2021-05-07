354ee636650747f3d3dc64c6c1b7298c2bf801fe
Adam Bien <abien@adam-bien.com>
2021-05-07 19:15:19 +0200

    CRUD events are created and distributed

--- mockend/src/main/java/airhacks/mockend/crud/boundary/CRUDResource.java
+++ mockend/src/main/java/airhacks/mockend/crud/boundary/CRUDResource.java
@@ -8,9 +8,11 @@ import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonValue;
 import javax.json.stream.JsonCollectors;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -23,11 +25,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+
+
 @Path("crud")
 @ApplicationScoped
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CRUDResource {
+
+    @Inject
+    @Channel("crud-events")
+    Emitter<JsonObject> emitter;
 
     Map<String, JsonObject> store;
     
@@ -39,29 +50,38 @@ public class CRUDResource {
     @GET
     @Path("{id}")
     public JsonObject find(@PathParam("id") String id) {
-        return this.store.get(id);
+        var result = this.store.get(id);
+        sendEvent("GET","/"+id, result);
+        return result;
     }
 
     @GET
     public JsonArray findAll() {
-        return this.store.values().stream().collect(JsonCollectors.toJsonArray());
+        var result = this.store.values().stream().collect(JsonCollectors.toJsonArray());
+        sendEvent("GET", "/",result);
+        return result;
     }
 
     @DELETE
     @Path("{id}")
     public void delete(@PathParam("id") String id) {
+        sendEvent("DELETE", "/"+id);
         this.store.remove(id);
     }
 
     @DELETE
     public void deleteAll() {
+        sendEvent("DELETE", "/");
         this.store.clear();
     }
 
     @PUT
     @Path("{id}")
     public Response upsert(@PathParam("id") String id, JsonObject input) {
-        var previous = this.store.put(id, addId(input,id));
+        var payload = addId(input, id);
+        var previous = this.store.put(id, payload);
+        sendEvent("POST", "/", payload);
+
         return previous == null ? Response.created(URI.create("/crud/"+id)).build() : Response.noContent().build();
 
     }
@@ -69,14 +89,44 @@ public class CRUDResource {
     @POST
     public Response insert(JsonObject input) {
         var generatedId = "" + System.currentTimeMillis();
-        this.store.put(generatedId, addId(input,generatedId));
+        var payload = addId(input, generatedId);
+        this.store.put(generatedId, payload);
+        sendEvent("POST", "/", payload);
         return Response.created(URI.create("/crud/" + generatedId)).build();
     }
 
-    JsonObject addId(JsonObject input,String id) {
-        return Json.createObjectBuilder(input).
-                add("id", id).
-        build();
+    JsonObject addId(JsonObject input, String id) {
+        return Json.createObjectBuilder(input).add("id", id).build();
+    }
+    
+    void sendEvent(String httpMethod, String path, JsonObject payload) {
+        var event = createEvent(httpMethod, path, payload);
+        emitter.send(event);
+    }
+
+    void sendEvent(String httpMethod, String path, JsonArray payload) {
+        var event = createEvent(httpMethod, path, payload);
+        emitter.send(event);
+    }
+
+    void sendEvent(String httpMethod, String path) {
+        var event = createEvent(httpMethod, path);
+        emitter.send(event);
+    }
+
+    JsonObject createEvent(String httpMethod, String path, JsonObject payload) {
+        var event = createEvent(httpMethod,path);
+        return Json.createObjectBuilder(event).add("payload", payload)
+                .build();
+    }
+    
+    JsonObject createEvent(String httpMethod, String path) {
+        return Json.createObjectBuilder().add("httpMethod", httpMethod).add("path", path).build();
+    }
+    
+    JsonObject createEvent(String httpMethod, String path, JsonArray payload) {
+        var wrapper = Json.createObjectBuilder().add("result", payload).build();
+        return this.createEvent(httpMethod, path, wrapper);
     }
 
 }
