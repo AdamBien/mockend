b442db2f746bdb64f0a0865676ac0873f0ca79ff
Adam Bien <abien@adam-bien.com>
2021-05-08 07:46:34 +0200

    user info improved

--- mockend/src/main/java/airhacks/mockend/convenience/boundary/UserInfo.java
+++ mockend/src/main/java/airhacks/mockend/convenience/boundary/UserInfo.java
@@ -22,12 +22,22 @@ public class UserInfo {
     @ConfigProperty(name="delay-in-ms", defaultValue="0")
     Long delayInMs;
 
+    @Inject
+    @ConfigProperty(name="quarkus.http.port")
+    int port;
+
     Logger LOG = Logger.getLogger(UserInfo.class.getName());
     
 
     @PostConstruct
     public void printMessages() {
-        LOG.info("----------------------------");
+        LOG.info("-----------usage------------");
+        LOG.info("swagger-ui: http://localhost:"+this.port+"/q/swagger-ui/");
+        LOG.info("create: curl -XPOST \"http://localhost:"+this.port+"/crud\" -H \"Content-Type: application/json\" -d \"{\"message\":\"hello, mockend\"}\"");
+        LOG.info("fetch with delay: curl -H\'delay-in-ms:1000\' http://localhost:"+this.port+"/crud/1618928997430");
+        LOG.info("echo: curl -X POST \"http://localhost:"+this.port+"/echo\" -H\"Content-Type: text/plain\" -d \"hello, mockend\"");
+        LOG.info("status: curl -H\"status: 500\" http://localhost:"+this.port+"/statuses");
+        LOG.info("sse: curl -N http://localhost:" + this.port + "/sse");
         LOG.info("You can use: delay-header-name property to change the header name, default is: delay-in-ms");
         LOG.info("use " + this.delayHeaderName + " header to delay all methods");
         LOG.info("use -Ddelay-in-ms=10 to set the delay from the CLI. Is going to be overriden by headers");
